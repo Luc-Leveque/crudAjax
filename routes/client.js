0031c012db6c1157ebb10ba3706cac42286ea18b
Luc leveque <luc.leveque@ynov.com>
2019-12-12 12:58:56 +0100

    init

---
+++ routes/client.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+module.exports = {
+    editClientPage: (req, res) => {
+        let clientId = req.params.id;
+
+        let query = "SELECT * FROM client WHERE id_client = '" + clientId + "' ";
+        db.query(query, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.render('edit-client.ejs', {
+                title: "Edit  Client"
+                ,client: result[0]
+                ,message: ''
+            });
+        });
+    },
+    editPlayer: (req, res) => {
+        let playerId = req.params.id;
+        let first_name = req.body.first_name;
+        let last_name = req.body.last_name;
+        let position = req.body.position;
+        let number = req.body.number;
+
+        let query = "UPDATE `players` SET `first_name` = '" + first_name + "', `last_name` = '" + last_name + "', `position` = '" + position + "', `number` = '" + number + "' WHERE `players`.`id` = '" + playerId + "'";
+        db.query(query, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.redirect('/');
+        });
+    },
+    deletePlayer: (req, res) => {
+        let playerId = req.params.id;
+        let getImageQuery = 'SELECT image from `players` WHERE id = "' + playerId + '"';
+        let deleteUserQuery = 'DELETE FROM players WHERE id = "' + playerId + '"';
+
+        db.query(getImageQuery, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+
+            let image = result[0].image;
+
+            fs.unlink(`public/assets/img/${image}`, (err) => {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+                db.query(deleteUserQuery, (err, result) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                    res.redirect('/');
+                });
+            });
+        });
+    }
+};
